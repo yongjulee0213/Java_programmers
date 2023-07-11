8ebb81485192e4980d8b29829fb1b991814ac601
Yongju Lee <72552929+yongjulee0213@users.noreply.github.com>
2023-07-11 16:51:27 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -0,0 +1,10 @@
+SELECT A.ID, A.NAME, B.HOST_ID
+FROM PLACES A
+INNER JOIN (
+            SELECT HOST_ID
+            FROM PLACES
+            GROUP BY HOST_ID
+            HAVING COUNT(*)>=2
+            )B
+ON A.HOST_ID=B.HOST_ID
+ORDER BY ID;
