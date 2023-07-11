7166b67e26802eaf6bf87839c7baf2a391bc230f
Yongju Lee <72552929+yongjulee0213@users.noreply.github.com>
2023-07-11 15:59:05 +0900

    [unrated] Title: 할 일 목록, Time: 0.03 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < todo_list.length; i++) {
+            if (!finished[i]) {
+                result.add(todo_list[i]);
+            }
+        }
+        
+        String[] answer = new String[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            answer[i] = result.get(i);
+        }
+        
+        return answer;
+    }
+}
